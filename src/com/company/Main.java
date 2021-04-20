22bc237cf116212074300c494d2f7078939501f9
VeronikaFenech <veronika.glow@gmail.com>
2021-04-21 00:10:20 +0300

    rename workers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	Worker worker= new Worker();
-	Worker worker1= new Worker();
-worker.work("Book");
-        worker1.work("Shoes");
+	Worker workers1= new Worker();
+	Worker workers2= new Worker();
+workers1.work("Book");
+        workers1.work("Shoes");
         System.out.println(" Worker 1 items");
-         for (String item: worker.getStorage().getItems()){
+         for (String item: workers1.getStorage().getItems()){
              System.out.print(item+ " ");
          }
         System.out.println();
         System.out.println(" Worker 2 items");
-        for (String item: worker1.getStorage().getItems()){
+        for (String item: workers2.getStorage().getItems()){
             System.out.print(item+ " ");
         }
         System.out.println();
